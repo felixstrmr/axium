c93697072c01d606eec5a16e669e50799c3b6ba0
strmfelix <email@felixstrohmeier.com>
2025-05-24 15:26:22 +0200

    feat: add database and authentication setup

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,8 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    DATABASE_URL: string;
+    DATABASE_URL: string
+    ENCRYPTION_KEY: string
+    BETTER_AUTH_SECRET: string
+    BETTER_AUTH_URL: string
   }
 }
