82dcf9b987bd0f7ea4e358bc2c0547d274df3831
strmfelix <email@felixstrohmeier.com>
2025-05-26 06:21:22 +0200

    feat: add settings section with sidebar navigation

---
+++ components/sidebars/settings-sidebar.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import { Separator } from '@/components/ui/separator'
+import { cn } from '@/utils'
+import { BookText, LucideIcon, User, Users } from 'lucide-react'
+import Link from 'next/link'
+import { useSelectedLayoutSegment } from 'next/navigation'
+
+export default function SettingsSidebar() {
+  const segment = useSelectedLayoutSegment()
+
+  const itemsAccount = [
+    {
+      name: 'Account',
+      href: '/settings',
+      icon: User,
+      isActive: segment === null,
+    },
+  ]
+
+  const itemsAdministration = [
+    {
+      name: 'General',
+      href: '/settings/general',
+      icon: BookText,
+      isActive: segment === 'general',
+    },
+    {
+      name: 'Users',
+      href: '/settings/users',
+      icon: Users,
+      isActive: segment === 'users',
+    },
+  ]
+
+  return (
+    <aside className='flex w-64 max-w-64 min-w-64 flex-col p-4'>
+      <div className='flex h-8 items-center'>
+        <h1 className='text-2xl font-semibold tracking-tight'>Settings</h1>
+      </div>
+      <Separator className='my-4' />
+      <div className='flex flex-col gap-1'>
+        {itemsAccount.map((item) => (
+          <SidebarItem key={item.name} {...item} />
+        ))}
+      </div>
+      <div className='flex flex-col gap-1'>
+        <h2 className='text-muted-foreground mb-1 text-xs'>Administration</h2>
+        {itemsAdministration.map((item) => (
+          <SidebarItem key={item.name} {...item} />
+        ))}
+      </div>
+    </aside>
+  )
+}
+
+type SidebarItemProps = {
+  name: string
+  href: string
+  icon: LucideIcon
+  isActive: boolean
+}
+
+function SidebarItem(item: SidebarItemProps) {
+  return (
+    <Link href={item.href}>
+      <div
+        className={cn(
+          'flex h-8 items-center gap-2 rounded-md p-2 transition-all',
+          item.isActive
+            ? 'bg-muted text-foreground'
+            : 'text-muted-foreground hover:bg-muted',
+        )}
+      >
+        <item.icon className='size-4' />
+        <span className='text-sm'>{item.name}</span>
+      </div>
+    </Link>
+  )
+}
